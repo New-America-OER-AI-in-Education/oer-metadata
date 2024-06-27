04b327b07a351a1ad623d33e3952fe08553c3295
Robin Ince <robince@gmail.com>
2024-06-27 14:11:35 -0400

    first PCA

--- scripts/explore_embeddings.py
+++ scripts/explore_embeddings.py
@@ -2,7 +2,9 @@
 import numpy as np
 import json
 from project import DATADIR
-
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 #%%
 with open(DATADIR / "embs_ids.json",'r',encoding="utf8") as f:
     resource_ids = json.load(f)
@@ -20,3 +22,18 @@ with open(resource_data_file, "r", encoding="utf16") as f:
         tuple(json.loads(k)): v for k, v in resource_data["authors"].items()
     }
 #%%
+# tsne
+model = TSNE(n_components=2, random_state=0)
+model.fit_transform(resource_embs[:5000,:])
+
+#%%
+# pca
+model = PCA(n_components=2, random_state=0)
+Xpca = model.fit_transform(resource_embs)
+
+#%%
+plt.scatter(Xpca[:,0], Xpca[:,1], s=1, alpha=0.1)
+
+#%% weird cluster
+idx = np.logical_and((Xpca[:,0]>0.45), (Xpca[:,1]>0.2))
+cluster_ids = [resource_ids[i] for i in np.where(idx)[0]]
